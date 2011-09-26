f11585e377458a964450b56937f18b5284035548
Syohei YOSHIDA <syohex@gmail.com>
2011-09-26 10:21:13 +0900

    init repos

---
+++ lib/List/UtilsBy/XS.pm
@@ -0,0 +1,41 @@
+package List::UtilsBy::XS;
+
+use 5.008_001;
+use strict;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+List::UtilsBy::XS -
+
+=head1 SYNOPSIS
+
+  use List::UtilsBy::XS;
+
+=head1 DESCRIPTION
+
+List::UtilsBy::XS is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
