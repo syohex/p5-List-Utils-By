88477e1c8737f165c6e32951bb91fae779b7d754
Syohei YOSHIDA <syohex@gmail.com>
2013-06-25 11:26:59 +0900

    add CFLAGS for older perls

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -13,6 +13,7 @@ sub new {
             './xs-src/UtilsBy.xs' => './lib/List/UtilsBy/XS.xs',
         },
         generate_ppport_h  => 'lib/List/UtilsBy/ppport.h',
+        extra_compiler_flags => ['-DPERL_EXT'],
         needs_compiler_c99 => 1,
     );
     return $self;
